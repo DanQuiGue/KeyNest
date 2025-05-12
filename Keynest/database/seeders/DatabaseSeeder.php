35a85d5e15bd1f6780fbe83589044084b771120b
DanQuiGue <daniel7394@gmail.com>
2025-05-12 20:40:25 +0200

    Insert User Resource

--- Keynest/database/seeders/DatabaseSeeder.php
+++ Keynest/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class DatabaseSeeder extends Seeder
         User::create([
             'name' => 'root',
             'email' => 'root@root.com',
-            'password' => Hash::make('root'),
+            'password' => Hash::make('12345678'),
         ]);
     }
 }
